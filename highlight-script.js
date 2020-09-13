e097314f10e0019522f3dc4877c6177554ae7fc6
Tomas Ye <tomaye@amazon.com>
2020-09-13 20:57:52 +0200

    [POC WORKS]

--- highlight-script.js
+++ highlight-script.js
@@ -3,14 +3,34 @@ function nativeClick(){
 }
 
 function nativeHttpCall(){
+  let content = document.getElementById('content-before').innerHTML;
+
+
   const Http = new XMLHttpRequest();
-  const url='http://127.0.0.1:5000/';
+  const url=`http://127.0.0.1:5000/get-keywords-for-text?body=${content}`;
   Http.open("GET", url);
   Http.send();
 
   Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+    let keywords = JSON.parse(Http.responseText);
+    console.log('Keywords = ')
+    console.log(keywords)
+    document.getElementById('content-after').innerHTML = transformContent(content, keywords);
   }
+}
+
+
+function transformContent(content, keywords){
+  let temp = content
+  Object.keys(keywords).forEach(key => {
+    temp = temp.replace(key, wrapKeywordWithLink(key, keywords[key]['link']))
+  })
+  return temp
+}
+
+
+function wrapKeywordWithLink(keyword, link){
+  return `<span style="font-weight: bold"> <a href="${link}" target="_blank"> ${keyword} </a> </span>`
 }
 
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -13,3 +14,37 @@ def hello_world():
 @app.route('/get-javascript')
 def get_javascript_file():
     return send_file('highlight-script.js', attachment_filename='highlight-script.js')
+
+
+@app.route('/get-keywords-for-text')
+def get_keywords():
+    result = {}
+    body = request.args.get('body')
+    keyword_dict = get_keyword_dict()
+    keys = set(keyword_dict.keys())
+
+    for word in body.split():
+        if word in keys:
+            result[word] = keyword_dict[word]
+
+    json_result = json.dumps(result, indent=2)
+    print(json_result)
+
+    return json_result
+
+
+def get_keyword_dict():
+    return {
+        "invest": {
+            "keyword": "invest",
+            "link": "https://en.wikipedia.org/wiki/Investment"
+        },
+        "interest": {
+            "keyword": "interest",
+            "link": "https://en.wikipedia.org/wiki/Interest"
+        },
+        "401k": {
+            "keyword": "401k",
+            "link": "https://en.wikipedia.org/wiki/401(k)"
+        }
+    }
